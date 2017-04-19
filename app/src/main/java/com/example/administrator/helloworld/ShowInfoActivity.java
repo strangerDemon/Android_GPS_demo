8ad6216f5419e9280ee631b65b8d87ef8702a241
strangerDemon <735359571@qq.com>
2017-04-19 08:58:34 +0800

    add dialog

--- app/src/main/java/com/example/administrator/helloworld/ShowInfoActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/ShowInfoActivity.java
@@ -1,15 +1,19 @@
 package com.example.administrator.helloworld;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.*;
 import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -34,7 +38,12 @@ public class ShowInfoActivity extends AppCompatActivity {
     public StringBuilder sb;//发送到显示面板的信息
     public static int count=0;//步数
     //网络
-    public  Socket socket;//
+    public static Socket socket;//
+    //调用窗口的状态
+    //时候打开对话窗口 当取消时不再弹出
+    public static boolean connectDialog=true;
+    public static boolean gpsDialog=true;
+    public static boolean permissionDialog=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,27 +59,29 @@ public class ShowInfoActivity extends AppCompatActivity {
             Bundle data = null;
             data = msg.getData();
             String show = "";
+            String type = "";
             switch (msg.what) {
-                case 1:
+                case 1://数据显示
                     show = (String) data.get("showinfo");
-                    System.out.println(show);
                     showinfo.setText(show);
-                    /*if(jsonString.trim().equals("1"))
-                    {
-                        System.out.println("短讯发送成功！");
+                    break;
+                case 2://弹窗
+                    type=(String) data.get("type");
+                    if( type == "connect") {
+                        isConnDialog();
+                    }else if(type == "permission"){
+                        isPermissionDialog();
+                    }else if(type == "gpsOpen"){
+                        isGPSOPenDialog();
                     }
-                    else{
-                        Toast toast = Toast.makeText(getApplicationContext(),
-                                jsonString, Toast.LENGTH_LONG);
-                        toast.setGravity(Gravity.CENTER, 0, 0);
-                        toast.show();
-                    }*/
                     break;
             }
             super.handleMessage(msg);
         }
     };
-    //android.os.NetworkOnMainThreadException是说不要在主线程中访问网络，
+    //android.os.NetworkOnMainThreadException是说不要在主线程中访问网络
+    // android 中 主线程不能访问网络
+    //            子线程不能修改ui
     // 这个是android3.0版本开始就强制程序不能在主线程中访问网络，要把访问网络放在独立的线程中。
     //启动线程
     public void progress() {
@@ -80,12 +91,10 @@ public class ShowInfoActivity extends AppCompatActivity {
     //自定义线程操作
     class beginThread extends Thread {
         public void run() {
-            //handler.sendEmptyMessage(0);
+            Looper.prepare();//相当于该线程Looper的初始化
             try {
-                createSocket();//创建连接
                 getGPS();//获取gps信息
             } catch (Exception e) {
-                //showinfo.setText(e.toString());
                 Message message=new Message();
                 Bundle bundle=new Bundle();
                 bundle.putString("showinfo",e.toString());
@@ -93,148 +102,88 @@ public class ShowInfoActivity extends AppCompatActivity {
                 message.what=1;
                 handler.sendMessage(message);
             }
+            Looper.loop();//Looper开始执行，注意该语句执行后，这个线程的其他操作就被阻塞，只能响应事件了。
         }
     }
 
     //获取gps信息 ，经度、纬度、速度、高度、方向
     private void getGPS(){
         while(true){
-            //获取定位服务，因为是系统的服务，因此不能new出来
+            //创建连接 防止 运行一半的时候 服务器挂了，用户关掉数据
+            createSocket();
+
             location=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            //如果未启动gps服务 启动gps服务
-            if(!location.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-                Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivityForResult(intent,0);
+            //获取定位服务，因为是系统的服务，因此不能new出来
+            isGPSOPen(location);
+
+            try {
+                if (Build.VERSION.SDK_INT >= 23) { //CONTROLLO PER ANDROID 6.0 O SUPERIORE
+                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)<0){//打开权限请求
+                        isPermission();
+                    }
+                } else {//android 6.0 以下
+                    location.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                    location.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                }
+            } catch (Exception ex) {
+                ex.toString();
             }
-            Looper.prepare();//相当于该线程Looper的初始化
+            location.removeUpdates(onLocationChanged);
             if(location.isProviderEnabled(LocationManager.GPS_PROVIDER)&&location.getLastKnownLocation(LocationManager.GPS_PROVIDER)!=null) {
                 updateTextView("GPS_PROVIDER",location.getLastKnownLocation(LocationManager.GPS_PROVIDER));
                 getSendData(location.getLastKnownLocation(LocationManager.GPS_PROVIDER));
                 //LocationManager.GPS_PROVIDER 定位服务提供者
                 //最小时间 单位 毫秒
                 //最小距离 单位 米
-                location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
-                    //当定位服务信息改变时
-                    @Override
-                    public void onLocationChanged(Location location1) {
-                        updateTextView("GPS_PROVIDER",location1);
-                        getSendData(location1);
-                    }
-                    //状态改变时
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-                    //gsp locationProvider 可用时
-                    @Override
-                    public void onProviderEnabled(String provider) {
-                        updateTextView("GPS_PROVIDER",location.getLastKnownLocation(provider));
-                        getSendData(location.getLastKnownLocation(provider));
-                    }
-                    //gsp locationProvider 不可用时
-                    @Override
-                    public void onProviderDisabled(String provider) {
-                        updateTextView("GPS_PROVIDER",null);
-                        getSendData(null);
-                    }
-                });
+                location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 2, onLocationChanged);
             }else if(location.isProviderEnabled(LocationManager.NETWORK_PROVIDER)&&location.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!=null){
                 updateTextView("NETWORK_PROVIDER",location.getLastKnownLocation(LocationManager.NETWORK_PROVIDER));
                 getSendData(location.getLastKnownLocation(LocationManager.NETWORK_PROVIDER));
-                location.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, new LocationListener() {
-                    //当定位服务信息改变时
-                    @Override
-                    public void onLocationChanged(Location location1) {
-                        updateTextView("NETWORK_PROVIDER",location1);
-                        getSendData(location1);
-                    }
-                    //状态改变时
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-                    //gsp locationProvider 可用时
-                    @Override
-                    public void onProviderEnabled(String provider) {
-                        updateTextView("NETWORK_PROVIDER",location.getLastKnownLocation(provider));
-                        getSendData(location.getLastKnownLocation(provider));
-                    }
-                    //gsp locationProvider 不可用时
-                    @Override
-                    public void onProviderDisabled(String provider) {
-                        updateTextView("NETWORK_PROVIDER",null);
-                        getSendData(null);
-                    }
-                });
+                location.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 2, onLocationChanged);
             }else{
                 updateTextView("PASSIVE_PROVIDER",location.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER));
                 getSendData(location.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER));
-                location.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 1000, 1, new LocationListener() {
-                    //当定位服务信息改变时
-                    @Override
-                    public void onLocationChanged(Location location1) {
-                        updateTextView("PASSIVE_PROVIDER",location1);
-                        getSendData(location1);
-                    }
-                    //状态改变时
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-                    //gsp locationProvider 可用时
-                    @Override
-                    public void onProviderEnabled(String provider) {
-                        updateTextView("PASSIVE_PROVIDER",location.getLastKnownLocation(provider));
-                        getSendData(location.getLastKnownLocation(provider));
-                    }
-                    //gsp locationProvider 不可用时
-                    @Override
-                    public void onProviderDisabled(String provider) {
-                        updateTextView("PASSIVE_PROVIDER",null);
-                        getSendData(null);
-                    }
-                });
+                location.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 2000, 2,onLocationChanged);
             }
             try {
-                beginThread.sleep(1000);
+                beginThread.sleep(2000);
             }catch(Exception ex){
 
             }
-            Looper.loop();//Looper开始执行，注意该语句执行后，这个线程的其他操作就被阻塞，只能响应事件了。
         }
-       /* //定位服务监听器，每隔1s，或者距离8更新获取一次
-        location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
-            //当定位服务信息改变时
-            @Override
-            public void onLocationChanged(Location location1) {
-                updateTextView(location1);
-                getSendData(location1);
-            }
-            //状态改变时
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-            //gsp locationProvider 可用时
-            @Override
-            public void onProviderEnabled(String provider) {
-                updateTextView(location.getLastKnownLocation(provider));
-                getSendData(location.getLastKnownLocation(provider));
-            }
-            //gsp locationProvider 不可用时
-            @Override
-            public void onProviderDisabled(String provider) {
-                updateTextView(null);
-                getSendData(null);
-            }
-        });*/
 
     }
+    //外部定义监测器
+    LocationListener onLocationChanged= new LocationListener() {
+        //当定位服务信息改变时
+        @Override
+        public void onLocationChanged(Location location1) {
+            updateTextView(location1.getProvider(),location1);
+            getSendData(location1);
+        }
+        //状态改变时
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+        //gsp locationProvider 可用时
+        @Override
+        public void onProviderEnabled(String provider) {
+            updateTextView(provider,location.getLastKnownLocation(provider));
+            getSendData(location.getLastKnownLocation(provider));
+        }
+        //gsp locationProvider 不可用时
+        @Override
+        public void onProviderDisabled(String provider) {
+            updateTextView(provider,null);
+            getSendData(null);
+        }
+    };
     //更新textview 上的显示
     private void updateTextView(String source,Location location){
         sb=new StringBuilder();
+        sb.append("来源"+source+"\n");
         if(location !=null){
-            sb.append("来源"+source+"\n");
             sb.append(count+"当前定位服务信息：\n");
             sb.append("经度："+location.getLongitude()+"\n");//经度
             sb.append("纬度："+location.getLatitude()+"\n");//纬度
@@ -243,17 +192,20 @@ public class ShowInfoActivity extends AppCompatActivity {
             sb.append("方向："+location.getBearing()+"\n");//方向
             sb.append("定位精度："+location.getAccuracy()+"\n");//定位精度
             sb.append("时间："+timedate(location.getTime()+"")+"\n");//时间
-            sb.append("getElapsedRealtimeNanos："+location.getElapsedRealtimeNanos()+"\n");//时间
         }else{
             sb.append("数据为空");
         }
-        //showinfo.setText(Html.fromHtml(sb.toString()));
+        //在线程中不能操作view
         Message message=new Message();
         Bundle bundle=new Bundle();
         bundle.putString("showinfo",sb.toString());
         message.setData(bundle);
         message.what=1;
-        handler.sendMessage(message);
+        try {
+            handler.sendMessage(message);
+        }catch(Exception ex){
+           updateTextView(ex.toString(),null);
+        }
     }
     //获取需要发送的信息数据 除了速度 坐标 其余都是假数据
     //数据格式：00006经纬度坐标        |心率|速度|步数 |步频|体温|配速|步幅
@@ -286,17 +238,20 @@ public class ShowInfoActivity extends AppCompatActivity {
     private void createSocket(){
         //1.创建客户端Socket，指定服务器地址和端口
         try {
+            isConn();
             if(socket==null) {
                 socket = new Socket("183.250.160.124", 8888);//
             }
         }catch(Exception ex){
-            Message message=new Message();
-            Bundle bundle=new Bundle();
-            bundle.putString("showinfo","网络连接失败："+ex.toString());
-            message.setData(bundle);
-            message.what=1;
-            handler.sendMessage(message);
-            //showinfo.setText("网络连接失败："+ex.toString());
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("服务器连接失败");
+            builder.setTitle("Warning");
+            builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
         }
     }
 
@@ -333,5 +288,129 @@ public class ShowInfoActivity extends AppCompatActivity {
         long lcc = Long.valueOf(time)+8*60*60*1000;//加8个小时 东八区
         String times = sdr.format(new Date(lcc));
         return times;
+    }
+
+    /*
+     * 判断网络连接是否已开
+     * true 已打开  false 未打开
+     */
+    public void isConn(){
+        boolean bisConnFlag=false;
+        ConnectivityManager conManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo network = conManager.getActiveNetworkInfo();
+        if(network==null) {
+            Message message = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putString("type", "connect");
+            message.setData(bundle);
+            message.what = 2;
+            handler.sendMessage(message);
+        }
+    }
+    //打开对应的弹窗
+    public void isConnDialog() {
+        AlertDialog.Builder connectDialog = new AlertDialog.Builder(ShowInfoActivity.this);
+        connectDialog.setMessage("网络连接未打开，是否开启？");
+        connectDialog.setTitle("Warning");
+        connectDialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                boolean bisConnFlag=false;
+                ConnectivityManager conManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo network = conManager.getActiveNetworkInfo();
+                if(network==null){
+                    //局域网
+                    Intent intent=new Intent(Settings.ACTION_WIFI_SETTINGS);
+                    startActivityForResult(intent,0);
+                    bisConnFlag=conManager.getActiveNetworkInfo().isAvailable();
+                    if(!bisConnFlag){//蜂窝网
+                        intent=new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS);
+                        startActivityForResult(intent,0);
+                    }
+                }
+            }
+
+        });
+        connectDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        // 显示
+        connectDialog.show();
+    }
+    /**
+     * 是否有权限
+     */
+    public void isPermission(){
+        Message message=new Message();
+        Bundle bundle=new Bundle();
+        bundle.putString("type","permission");
+        message.setData(bundle);
+        message.what=2;
+        handler.sendMessage(message);
+    }
+    public void isPermissionDialog(){
+        AlertDialog.Builder permissionDialog = new AlertDialog.Builder(ShowInfoActivity.this);
+        permissionDialog.setMessage("应用的位置权限未打开，是否打开？");
+        permissionDialog.setTitle("Warning");
+        permissionDialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Uri packageURI = Uri.parse("package:" + getPackageName());
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+                startActivity(intent);
+            }
+
+        });
+        permissionDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        // 显示
+        permissionDialog.show();
+    }
+    /**
+     * gps 是否打开
+     */
+    public void isGPSOPen(LocationManager location){
+        //如果未启动gps服务 启动gps服务
+        if(!location.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Message message=new Message();
+            Bundle bundle=new Bundle();
+            bundle.putString("type","gpsOpen");
+            message.setData(bundle);
+            message.what=2;
+            handler.sendMessage(message);
+        }
+    }
+    //打开弹窗
+    public void isGPSOPenDialog(){
+        AlertDialog.Builder gpsOpenDialog = new AlertDialog.Builder(ShowInfoActivity.this);
+        gpsOpenDialog.setMessage("GPS服务未打开，是否打开以提高精度？");
+        gpsOpenDialog.setTitle("Warning");
+        gpsOpenDialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(intent,0);
+            }
+        });
+        gpsOpenDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                //finish();
+            }
+        });
+        // 显示
+        gpsOpenDialog.show();
     }
 }
