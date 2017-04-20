47254fd6e47f1c792b3b75621905d740ecbca056
strangerDemon <735359571@qq.com>
2017-04-20 11:56:46 +0800

    a demo of Pedometer  find by baidu

---
+++ app/src/main/java/com/example/administrator/helloworld/step/StepCallBack.java
@@ -0,0 +1,11 @@
+package com.example.administrator.helloworld.step;
+
+/**
+ * Created by base on 2016/8/17.
+ */
+public interface StepCallBack {
+    /**
+     * 计步回调
+     */
+    public void Step(int stepNum);
+}
